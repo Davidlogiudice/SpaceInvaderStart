6af2408ae2b4dbc93dac8aa72dee9a086e9b7707
Davidlogiudice <71661173+Davidlogiudice@users.noreply.github.com>
2020-10-05 12:08:44 +0200

    Add files via upload

--- spaceInvader.js
+++ spaceInvader.js
@@ -20,47 +20,46 @@ function spriteCharacter() {
     context.closePath();
 
     context.beginPath();
-    context.fillStyle = "green";
+    context.fillStyle = "#00FF00";
     context.rect(150, 50, 150, 50, 0);
     context.fill();
     context.closePath();
 
     context.beginPath();
-    context.fillStyle = "green";
+    context.fillStyle = "#00FF00";
     context.rect(100, 100, 100, 50, 0);
     context.fill();
     context.closePath();
 
     context.beginPath();
-    context.fillStyle = "green";
+    context.fillStyle = "#00FF00";
     context.rect(250, 100, 100, 50, 0);
     context.fill();
     context.closePath();
 
     context.beginPath();
-    context.fillStyle = "green";
+    context.fillStyle = "#00FF00";
     context.rect(200, 150, 50, 50, 0);
     context.fill();
     context.closePath();
 
     context.beginPath();
-    context.fillStyle = "green";
+    context.fillStyle = "#00FF00";
     context.rect(100, 200, 50, 50, 0);
     context.fill();
     context.closePath();
 
     context.beginPath();
-    context.fillStyle = "green";
+    context.fillStyle = "#00FF00";
     context.rect(300, 200, 50, 50, 0);
     context.fill();
     context.closePath();
 
-    
+
     context.beginPath();
-    context.fillStyle = "green";
+    context.fillStyle = "#00FF00";
     context.rect(100, 250, 250, 50, 0);
     context.fill();
     context.closePath();
 
 }
-
